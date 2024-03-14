44507ecb50757fb16626e1a4c70d764f7141e34b
Gargi M <77939566+GargiM99@users.noreply.github.com>
2024-03-14 15:12:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-/**
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.57.1"
-    }
-  }
-}
-**/
-
 provider "aws" {
   region                   = "us-east-1"
   shared_credentials_files = ["./credentials.txt"]
